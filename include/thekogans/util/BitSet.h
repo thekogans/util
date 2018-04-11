806ef0dc58e18823a264410869a6408deb5b80de
Boris Kogan <bkogan@logitech.com>
2018-04-11 14:12:26 -0700

    Updated BitSet to closely resemble the std::bitset api.

--- include/thekogans/util/BitSet.h
+++ include/thekogans/util/BitSet.h
@@ -21,6 +21,7 @@
 #include <vector>
 #include "thekogans/util/Config.h"
 #include "thekogans/util/Types.h"
+#include "thekogans/util/Serializer.h"
 
 namespace thekogans {
     namespace util {
@@ -29,51 +30,303 @@ namespace thekogans {
         ///
         /// \brief
         /// BitSet is meant to be an extension of \see{Flags}. If your needs are
-        /// simple (basically an array of flags), BitSet will do the trick.
+        /// simple (basically an array of flags), BitSet will do the trick. It's
+        /// designed to have an interface compatible with std::bitset but it's
+        /// not a template. You can resize BitSet at runtime.
 
         struct _LIB_THEKOGANS_UTIL_DECL BitSet {
             /// \brief
             /// The bit set.
-            /// Most significant bit of bits[0] is bit 0.
             std::vector<ui32> bits;
+            /// \brief
+            /// Size of the bit set.
+            std::size_t size;
 
             /// \brief
             /// ctor.
-            /// \param[in] size Number of bits in the bit set.
-            BitSet (ui32 size = 0) {
-                Resize (size);
+            /// \param[in] size_ Number of bits in the bit set.
+            BitSet (std::size_t size_ = 0) :
+                    size (0) {
+                Resize (size_);
+            }
+
+            /// \brief
+            /// Return the size of the bit set.
+            /// \return Size of the bit set.
+            inline std::size_t Size () const {
+                return size;
             }
 
             /// \brief
             /// Resize and clear the bit set.
-            /// \param[in] size Number of bits in the new bit set.
+            /// \param[in] size_ Number of bits in the new bit set.
             /// VERY IMPORTANT: Resize does not preserve the old
             /// contents. In fact, it clears the new bit set to 0.
-            void Resize (ui32 size);
+            void Resize (std::size_t size_);
 
             /// \brief
             /// Return true if the bit at the given position is set.
             /// \param[in] bit The bit position to test.
             /// \return true = bit == 1, false = bit == 0.
-            bool Test (ui32 bit) const;
+            bool Test (std::size_t bit) const;
             /// \brief
             /// Set or clear the bit at the given position.
             /// \param[in] bit The bit position to set/clear.
             /// \param[in] on true = set, false = clear.
             /// \return Previous bit value.
             bool Set (
-                ui32 bit,
+                std::size_t bit,
                 bool on);
             /// \brief
             /// Flip the bit at the given position.
             /// \param[in] bit The bit position to flip.
             /// \return Previous bit value.
-            bool Flip (ui32 bit);
+            bool Flip (std::size_t bit);
 
+            /// \brief
+            /// Set all bits to 1.
+            void Set ();
             /// \brief
             /// Set all bits to 0.
             void Clear ();
+            /// \brief
+            /// Flip all bits (0 -> 1, 1 -> 0).
+            void Flip ();
+
+            /// \brief
+            /// Return count of 1 bits.
+            /// \return count of 1 bits.
+            std::size_t Count () const;
+            /// \brief
+            /// Return true if any bits are set.
+            /// \return true if any bits are set.
+            bool Any () const;
+            /// \brief
+            /// Return true if no bits are set.
+            /// \return true if no bits are set.
+            bool None () const;
+            /// \brief
+            /// Return true if all bits are set.
+            /// \return true if all bits are set.
+            bool All () const;
+
+            /// \brief
+            /// r-value [] operator.
+            /// \param[in] bit Which bit to test.
+            /// \return true == bit == 1, false == bit == 0.
+            inline bool operator [] (std::size_t bit) const {
+                return Test (bit);
+            }
+            /// \struct BitSet::Proxy BitSet.h thekogans/util/BitSet.h
+            ///
+            /// \brief
+            /// Proxy class for l-value [] operator.
+            struct Proxy {
+                /// \brief
+                /// Set the bit to on.
+                /// \param[in] on true == set, false == reset.
+                /// \return *this.
+                Proxy &operator = (bool on) {
+                    bitSet->Set (bit, on);
+                    return *this;
+                }
+
+                /// \brief
+                /// Set the bit to the value of another proxy.
+                /// \param[in] proxy Proxy to test.
+                /// \return *this.
+                Proxy &operator = (const Proxy &proxy) {
+                    bitSet->Set (bit, bool (proxy));
+                    return *this;
+                }
+
+                /// \brief
+                /// Flip the bit from 0 -> 1, 1 -> 0.
+                /// \return *this.
+                Proxy &Flip () {
+                    bitSet->Flip (bit);
+                    return *this;
+                }
+
+                /// \brief
+                /// Return true if bit is not set, false if set.
+                /// \return true if bit is not set, false if set.
+                bool operator ~ () const {
+                    return !bitSet->Test (bit);
+                }
+
+                /// \brief
+                /// Return true if the given bit is set.
+                /// \return true if the given bit is set.
+                operator bool () const {
+                    return bitSet->Test (bit);
+                }
+
+            private:
+                /// \brief
+                /// ctor.
+                Proxy () :
+                    bitSet (0),
+                    bit (0) {}
+
+                /// \brief
+                /// ctor.
+                /// \param[in] bitSet_ BitSet to proxy.
+                /// \param[in] bit_ Bit position to proxy.
+                Proxy (
+                    BitSet *bitSet_,
+                    std::size_t bit_) :
+                    bitSet (bitSet_),
+                    bit (bit_) {}
+
+                /// \brief
+                /// BitSet to proxy.
+                BitSet *bitSet;
+                /// \brief
+                /// Bit position to proxy.
+                std::size_t bit;
+
+                /// \brief
+                /// Needs access to provate ctors.
+                friend struct BitSet;
+            };
+            /// \brief
+            /// l-value [] operator.
+            /// \param[in] bit Which bit to set.
+            /// \return Proxy to set the bit.
+            Proxy operator [] (std::size_t bit);
+
+            /// \brief
+            /// And the given bit set to this one.
+            /// \param[in] bitSet BitSet to and with this one.
+            /// \return *this.
+            BitSet &operator &= (const BitSet &bitSet);
+            /// \brief
+            /// Or the given bit set to this one.
+            /// \param[in] bitSet BitSet to or with this one.
+            /// \return *this.
+            BitSet &operator |= (const BitSet &bitSet);
+            /// \brief
+            /// Xor the given bit set to this one.
+            /// \param[in] bitSet BitSet to xor with this one.
+            /// \return *this.
+            BitSet &operator ^= (const BitSet &bitSet);
+
+            /// \brief
+            /// Shift the entire bit set to the left.
+            /// \param[in] count Number of bits to shift.
+            /// \return *this
+            BitSet &operator <<= (std::size_t count);
+            /// \brief
+            /// Shift the entire bit set to the right.
+            /// \param[in] count Number of bits to shift.
+            /// \return *this
+            BitSet &operator >>= (std::size_t count);
+
+            /// \brief
+            /// Return the Not bit set.
+            /// \return Not (0 bits become 1, and 1 bits become 0) of the bit set.
+            BitSet operator ~ () const;
+
+            /// \brief
+            /// Return a copy of the bit set shifted left by the given count.
+            /// \param[in] count Number of bits to shift left.
+            /// \return A copy of the bit set shifted left by the given count.
+            inline BitSet operator << (std::size_t count) const {
+                return BitSet (*this) <<= count;
+            }
+
+            /// \brief
+            /// Return a copy of the bit set shifted right by the given count.
+            /// \param[in] count Number of bits to shift right.
+            /// \return A copy of the bit set shifted right by the given count.
+            inline BitSet operator >> (std::size_t count) const {
+                return BitSet (*this) >>= count;
+            }
+
+        private:
+            /// \brief
+            /// Per the standard, the unused bits need to be cleared to 0.
+            /// This function accomplishes that and is called by various methods.
+            void Trim ();
         };
+
+        /// \brief
+        /// And two bit sets together.
+        /// \param[in] bitSet1 First bit set to and.
+        /// \param[in] bitSet2 Second bit set to and.
+        /// \return bitSet1 & bitSet2.
+        inline BitSet operator & (
+                const BitSet &bitSet1,
+                const BitSet &bitSet2) {
+            BitSet temp = bitSet1;
+            return temp &= bitSet2;
+		}
+
+        /// \brief
+        /// Or two bit sets together.
+        /// \param[in] bitSet1 First bit set to or.
+        /// \param[in] bitSet2 Second bit set to or.
+        /// \return bitSet1 | bitSet2.
+        inline BitSet operator | (
+                const BitSet &bitSet1,
+                const BitSet &bitSet2) {
+            BitSet temp = bitSet1;
+            return temp |= bitSet2;
+		}
+
+        /// \brief
+        /// Xor two bit sets together.
+        /// \param[in] bitSet1 First bit set to xor.
+        /// \param[in] bitSet2 Second bit set to xor.
+        /// \return bitSet1 ^ bitSet2.
+        inline BitSet operator ^ (
+                const BitSet &bitSet1,
+                const BitSet &bitSet2) {
+            BitSet temp = bitSet1;
+            return temp ^= bitSet2;
+		}
+
+        /// \brief
+        /// Return true if bitSet1 == bitSet2.
+        /// \return true if bitSet1 == bitSet2.
+        _LIB_THEKOGANS_UTIL_DECL bool _LIB_THEKOGANS_UTIL_API operator == (
+            const BitSet &bitSet1,
+            const BitSet &bitSet2);
+
+        /// \brief
+        /// Return true if bitSet1 != bitSet2.
+        /// \return true if bitSet1 != bitSet2.
+        _LIB_THEKOGANS_UTIL_DECL bool _LIB_THEKOGANS_UTIL_API operator != (
+            const BitSet &bitSet1,
+            const BitSet &bitSet2);
+
+        /// \brief
+        /// Write the given bit set to the given serializer.
+        /// \param[in] serializer Where to write the given bit set.
+        /// \param[in] bitSet BitSet to write.
+        /// \return serializer.
+        inline Serializer &operator << (
+                Serializer &serializer,
+                const BitSet &bitSet) {
+            ui32 size = (ui32)bitSet.size;
+            serializer << bitSet.bits << size;
+            return serializer;
+        }
+
+        /// \brief
+        /// Read an bit set from the given serializer.
+        /// \param[in] serializer Where to read the bit set from.
+        /// \param[out] bitSet BitSet to read.
+        /// \return serializer.
+        inline Serializer &operator >> (
+                Serializer &serializer,
+                BitSet &bitSet) {
+            ui32 size;
+            serializer >> bitSet.bits >> size;
+            bitSet.size = size;
+            return serializer;
+        }
 
     } // namespace util
 } // namespace thekogans
--- src/BitSet.cpp
+++ src/BitSet.cpp
@@ -16,45 +16,273 @@
 // along with libthekogans_util. If not, see <http://www.gnu.org/licenses/>.
 
 #include <cstring>
+#include "thekogans/util/Exception.h"
 #include "thekogans/util/BitSet.h"
 
 namespace thekogans {
     namespace util {
 
-        void BitSet::Resize (ui32 size) {
-            if (size == 0) {
-                bits.clear ();
-            }
-            else {
-                bits.resize ((size + 31) >> 5);
-                Clear ();
+        void BitSet::Resize (std::size_t size_) {
+            if (size != size_) {
+                if (size_ == 0) {
+                    bits.clear ();
+                }
+                else {
+                    bits.resize ((size_ + 31) >> 5);
+                    Clear ();
+                }
+                size = size_;
             }
         }
 
-        bool BitSet::Test (ui32 bit) const {
-            return ((bits[bit >> 5] >> (31 - (bit & 31))) & 1) != 0;
+        bool BitSet::Test (std::size_t bit) const {
+            if (bit < size) {
+                return ((bits[bit >> 5] >> (31 - (bit & 31))) & 1) != 0;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
         }
 
         bool BitSet::Set (
-                ui32 bit,
+                std::size_t bit,
                 bool on) {
-            bool old = Test (bit);
-            static const ui32 mask = 0x80000000;
-            if (on) {
-                bits[bit >> 5] |= mask >> (bit & 31);
+            if (bit < size) {
+                bool old = Test (bit);
+                static const ui32 mask = 0x80000000;
+                if (on) {
+                    bits[bit >> 5] |= mask >> (bit & 31);
+                }
+                else {
+                    bits[bit >> 5] &= ~(mask >> (bit & 31));
+                }
+                return old;
             }
             else {
-                bits[bit >> 5] &= ~(mask >> (bit & 31));
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
             }
-            return old;
         }
 
-        bool BitSet::Flip (ui32 bit) {
-            return Set (bit, !Test (bit));
+        bool BitSet::Flip (std::size_t bit) {
+            if (bit < size) {
+                return Set (bit, !Test (bit));
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
+        }
+
+        void BitSet::Set () {
+            if (size > 0) {
+                memset (&bits[0], 0xff, bits.size () * UI32_SIZE);
+            }
+            else {
+                THEKOGANS_UTIL_THROW_STRING_EXCEPTION (
+                    "%s", "BitSet is empty.");
+            }
         }
 
         void BitSet::Clear () {
-            memset (&bits[0], 0, bits.size () * UI32_SIZE);
+            if (size > 0) {
+                memset (&bits[0], 0, bits.size () * UI32_SIZE);
+            }
+            else {
+                THEKOGANS_UTIL_THROW_STRING_EXCEPTION (
+                    "%s", "BitSet is empty.");
+            }
+        }
+
+        namespace {
+        }
+
+        void BitSet::Flip () {
+            for (std::size_t i = 0, count = bits.size (); i < count; ++i) {
+                bits[i] = ~bits[i];
+            }
+            Trim ();
+        }
+
+        std::size_t BitSet::Count () const {
+            std::size_t count = 0;
+            if (size > 0) {
+                static const char * const bitsPerByte =
+                    "\0\1\1\2\1\2\2\3\1\2\2\3\2\3\3\4"
+                    "\1\2\2\3\2\3\3\4\2\3\3\4\3\4\4\5"
+                    "\1\2\2\3\2\3\3\4\2\3\3\4\3\4\4\5"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\1\2\2\3\2\3\3\4\2\3\3\4\3\4\4\5"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\3\4\4\5\4\5\5\6\4\5\5\6\5\6\6\7"
+                    "\1\2\2\3\2\3\3\4\2\3\3\4\3\4\4\5"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\3\4\4\5\4\5\5\6\4\5\5\6\5\6\6\7"
+                    "\2\3\3\4\3\4\4\5\3\4\4\5\4\5\5\6"
+                    "\3\4\4\5\4\5\5\6\4\5\5\6\5\6\6\7"
+                    "\3\4\4\5\4\5\5\6\4\5\5\6\5\6\6\7"
+                    "\4\5\5\6\5\6\6\7\5\6\6\7\6\7\7\x8";
+                const ui8 *begin = (const ui8 *)(const void *)&bits[0];
+                const ui8 *end = begin + (bits.size () << 2);
+                for (; begin != end; ++begin) {
+                    count += bitsPerByte[*begin];
+                }
+            }
+            return count;
+        }
+
+        bool BitSet::Any () const {
+            for (std::size_t i = 0, count = bits.size (); i < count; ++i) {
+                if (bits[i] != 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool BitSet::None () const {
+            return !Any ();
+        }
+
+        bool BitSet::All () const {
+            return Count () == Size ();
+        }
+
+        BitSet::Proxy BitSet::operator [] (std::size_t bit) {
+            if (bit < size) {
+                return Proxy (this, bit);
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
+        }
+
+        BitSet &BitSet::operator &= (const BitSet &bitSet) {
+            if (size == bitSet.size) {
+                for (std::size_t i = 0, count = bits.size (); i < count; ++i) {
+                    bits[i] &= bitSet.bits[i];
+                }
+                return *this;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
+        }
+
+        BitSet &BitSet::operator |= (const BitSet &bitSet) {
+            if (size == bitSet.size) {
+                for (std::size_t i = 0, count = bits.size (); i < count; ++i) {
+                    bits[i] |= bitSet.bits[i];
+                }
+                return *this;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
+        }
+
+        BitSet &BitSet::operator ^= (const BitSet &bitSet) {
+            if (size == bitSet.size) {
+                for (std::size_t i = 0, count = bits.size (); i < count; ++i) {
+                    bits[i] ^= bitSet.bits[i];
+                }
+                return *this;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_ERROR_CODE_EXCEPTION (
+                    THEKOGANS_UTIL_OS_ERROR_CODE_EINVAL);
+            }
+        }
+
+        BitSet &BitSet::operator <<= (std::size_t count) {
+            if (size > 0) {
+                std::size_t wordCount = count / 32;
+                if (wordCount != 0) {
+                    for (std::size_t i = bits.size (); --i >= 0;) {
+                        bits[i] = wordCount <= i ? bits[i - wordCount] : 0;
+                    }
+                }
+                if ((count %= 32) != 0) {
+                    for (std::size_t i = bits.size (); --i > 0;) {
+                        bits[i] = (bits[i] << count) | (bits[i - 1] >> (32 - count));
+                    }
+                    bits[0] <<= count;
+                }
+                Trim ();
+                return *this;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_STRING_EXCEPTION (
+                    "%s", "BitSet is empty.");
+            }
+        }
+
+        BitSet &BitSet::operator >>= (std::size_t count) {
+            if (size > 0) {
+                std::size_t wordCount = count / 32;
+                if (wordCount != 0) {
+                    for (std::size_t i = 0, last = bits.size (); i <= last; ++i) {
+                        bits[i] = wordCount <= last - i - 1 ? bits[i + wordCount] : 0;
+                    }
+                }
+                if ((count %= 32) != 0) {
+                    for (std::size_t i = 0, last = bits.size () - 1; i < last; ++i) {
+                        bits[i] = (bits[i] >> count) | (bits[i + 1] << (32 - count));
+                    }
+                    bits.back () >>= count;
+                }
+                return *this;
+            }
+            else {
+                THEKOGANS_UTIL_THROW_STRING_EXCEPTION (
+                    "%s", "BitSet is empty.");
+            }
+        }
+
+        BitSet BitSet::operator ~ () const {
+            BitSet bitSet (*this);
+            bitSet.Flip ();
+            return bitSet;
+        }
+
+        void BitSet::Trim () {
+            if (size % 32 != 0) {
+                bits.back () &= ((ui32)1 << size % 32) - 1;
+            }
+        }
+
+        _LIB_THEKOGANS_UTIL_DECL bool _LIB_THEKOGANS_UTIL_API operator == (
+                const BitSet &bitSet1,
+                const BitSet &bitSet2) {
+            if (bitSet1.size == bitSet2.size) {
+                for (std::size_t i = 0, count = bitSet1.bits.size (); i < count; ++i) {
+                    if (bitSet1.bits[i] != bitSet2.bits[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        _LIB_THEKOGANS_UTIL_DECL bool _LIB_THEKOGANS_UTIL_API operator != (
+                const BitSet &bitSet1,
+                const BitSet &bitSet2) {
+            if (bitSet1.size == bitSet2.size) {
+                for (std::size_t i = 0, count = bitSet1.bits.size (); i < count; ++i) {
+                    if (bitSet1.bits[i] != bitSet2.bits[i]) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return true;
         }
 
     } // namespace util
