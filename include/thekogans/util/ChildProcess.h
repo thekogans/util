34b04759d77e2f7d2f372f33b13e005aac54b845
Boris Kogan <boris@thekogans.net>
2020-03-12 14:01:31 -0700

    Minor comment fix.

--- include/thekogans/util/ChildProcess.h
+++ include/thekogans/util/ChildProcess.h
@@ -395,7 +395,7 @@ namespace thekogans {
             /// using namespace thekogans;
             ///
             /// util::ChildProcess lsProcess ("ls");
-            /// util::MainRunLoop::Instance ().Enq (lsProcess.CreateSpawnJob (), true);
+            /// util::MainRunLoop::Instance ().EnqJob (lsProcess.CreateSpawnJob (), true);
             /// if (lsProcess.GetProcessId () != THEKOGANS_UTIL_INVALID_PROCESS_ID_VALUE) {
             ///     util::Buffer::UniquePtr lsOutput = ls.CollectOutput (ls.GetOutPipe ());
             /// }
